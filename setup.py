3c4c916eadb9fdef1c762491ca40d726f3575258
Daniel Lepage <dplepage@gmail.com>
2013-08-15 14:08:25 -0400

    Move README to RST, bump version #.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name='dfiance',
-    version='0.1.2',
+    version='0.1.3',
     license='BSD',
     author="Daniel Lepage",
     author_email="dplepage@gmail.com",
     packages=['dfiance',],
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 2",
